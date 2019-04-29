2eb9ee24e7fc19ea9f6299e0b8f542b3500f0efb
Dario Enrique Martinez Zabala <info@sitedigital.com.co>
2019-04-29 18:15:35 -0500

    Ajustes web
    Ajustes web

--- src/ComercialServiceProvider.php
+++ src/ComercialServiceProvider.php
@@ -4,24 +4,18 @@ namespace Digitalsite\Comercial;
 
 use Illuminate\Support\ServiceProvider;
 
-/**
-* 
-*/
-class ComercialServiceProvider extends ServiceProvider
-{
-	
+class ComercialServiceProvider extends ServiceProvider{
+ public function register(){
+  $this->app->bind('comercial', function($app) {
+  return new Comercial;
+  });
+ }
 
-	public function boot(){
-	 include __DIR__ . '/Http/routes.php';
-      
-     $this->loadViewsFrom(__DIR__ . '/../views', 'comercial');
-    }
-
-	public function register(){
-	 
-	 $this->app['comercial'] = $this->app->share(function ($app){
-	 	return new Comercial;
-	 });
-}
-
-}
+ public function boot(){
+  require __DIR__ . '/Http/routes.php';
+  $this->loadViewsFrom(__DIR__ . '/../views', 'comercial');
+  $this->publishes([
+   __DIR__ . '/migrations/2015_07_25_000000_create_usuario_table.php' => base_path('database/migrations/2015_07_25_000000_create_usuario_table.php'),
+  ]);
+  }
+ }
